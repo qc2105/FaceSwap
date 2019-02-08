fc08acae73ef7f4a8861e44b1843f4d5808f4571
Chuan Qin <qc2105@qq.com>
2019-02-08 12:01:26 +0800

    Capture video from mp4 and run with python3

--- FaceSwap/NonLinearLeastSquares.py
+++ FaceSwap/NonLinearLeastSquares.py
@@ -1,9 +1,11 @@
 import numpy as np
 from scipy import optimize
 
+
 def LineSearchFun(alpha, x, d, fun, args):
     r = fun(x + alpha * d, *args)
     return np.sum(r**2)
+
 
 def GaussNewton(x0, fun, funJack, args, maxIter=10, eps=10e-7, verbose=1):
     x = np.array(x0, dtype=np.float64)
@@ -14,7 +16,7 @@ def GaussNewton(x0, fun, funJack, args, maxIter=10, eps=10e-7, verbose=1):
         cost = np.sum(r**2)
 
         if verbose > 0:
-            print "Cost at iteration " + str(i) + ": " + str(cost)
+            print("Cost at iteration " + str(i) + ": " + str(cost))
 
         if (cost < eps or abs(cost - oldCost) < eps):
             break
@@ -25,21 +27,24 @@ def GaussNewton(x0, fun, funJack, args, maxIter=10, eps=10e-7, verbose=1):
         H = np.dot(J.T, J)
         direction = np.linalg.solve(H, grad)
 
-        #optymalizacja dlugosci kroku
-        lineSearchRes = optimize.minimize_scalar(LineSearchFun, args=(x, direction, fun, args))
-        #dlugosc kroku
+        # optymalizacja dlugosci kroku
+        lineSearchRes = optimize.minimize_scalar(LineSearchFun,
+                                                 args=(x, direction, fun, args)
+                                                 )
+        # dlugosc kroku
         alpha = lineSearchRes["x"]
 
         x = x + alpha * direction
-        
+
     if verbose > 0:
-        print "Gauss Netwon finished after "  + str(i + 1) + " iterations"
+        print("Gauss Netwon finished after " + str(i + 1) + " iterations")
         r = fun(x, *args)
         cost = np.sum(r**2)
-        print "cost = " + str(cost)
-        print "x = " + str(x)
+        print("cost = " + str(cost))
+        print("x = " + str(x))
 
     return x
+
 
 def SteepestDescent(x0, fun, funJack, args, maxIter=10, eps=10e-7, verbose=1):
     x = np.array(x0, dtype=np.float64)
@@ -50,9 +55,9 @@ def SteepestDescent(x0, fun, funJack, args, maxIter=10, eps=10e-7, verbose=1):
         cost = np.sum(r**2)
 
         if verbose > 0:
-            print "Cost at iteration " + str(i) + ": " + str(cost)
+            print("Cost at iteration " + str(i) + ": " + str(cost))
 
-        #warunki stopu
+        # warunki stopu
         if (cost < eps or abs(cost - oldCost) < eps):
             break
         oldCost = cost
@@ -61,20 +66,20 @@ def SteepestDescent(x0, fun, funJack, args, maxIter=10, eps=10e-7, verbose=1):
         grad = 2 * np.dot(J.T, r)
         direction = grad
 
-        #optymalizacja dlugosci kroku
-        lineSearchRes = optimize.minimize_scalar(LineSearchFun, args=(x, direction, fun, args))
-        #dlugosc kroku
+        # optymalizacja dlugosci kroku
+        lineSearchRes = optimize.minimize_scalar(LineSearchFun,
+                                                 args=(x, direction, fun, args)
+                                                 )
+        # dlugosc kroku
         alpha = lineSearchRes["x"]
 
         x = x + alpha * direction
 
     if verbose > 0:
-        print "Steepest Descent finished after "  + str(i + 1) + " iterations"
+        print("Steepest Descent finished after " + str(i + 1) + " iterations")
         r = fun(x, *args)
         cost = np.sum(r**2)
-        print "cost = " + str(cost)
-        print "x = " + str(x)
-
+        print("cost = " + str(cost))
+        print("x = " + str(x))
 
     return x
-
